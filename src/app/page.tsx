6f8d33e9e34582c389697a2838a236a5c6425687
Daniela <martinezdl_@hotmail.com>
2023-12-12 17:23:41 -0300

    12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ import { useState, useEffect } from 'react'
 import Image from 'next/image';
 
 export default function Home() {
-
-
   const [gifts, setGifts] = useState(() => {
     const storedGifts = localStorage.getItem('gifts');
     return storedGifts ? JSON.parse(storedGifts) : [
-      { name: "Viaje a Brasil", quantity: 1, imageUrl: "https://dib.com.ar/wp-content/uploads/2022/10/rio-janeiro-696x870.jpg" }
+      { recipient: "Dani", name: "Viaje a Brasil", quantity: 1, imageUrl: "https://dib.com.ar/wp-content/uploads/2022/10/rio-janeiro-696x870.jpg" }
     ];
   });
   const [newGift, setNewGift] = useState('');
-  const [newImageUrl, setNewImageUrl] = useState(''); // Add state for the image URL
+  const [person, setPerson] = useState('');
+  const [newImageUrl, setNewImageUrl] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     localStorage.setItem('gifts', JSON.stringify(gifts));
@@ -32,11 +32,13 @@ export default function Home() {
         updatedGifts[existingGiftIndex].quantity += 1;
         setGifts(updatedGifts);
       } else {
-        setGifts([...gifts, { name: newGift, quantity: 1, imageUrl: newImageUrl }]);
+        setGifts([...gifts, { name: newGift, quantity: 1, imageUrl: newImageUrl, recipient: person }]);
       }
 
       setNewGift('');
       setNewImageUrl('');
+      setIsModalOpen(false);
+      setPerson('')
       console.log(gift);
     }
   };
@@ -60,35 +62,57 @@ export default function Home() {
     setGifts([])
   }
   return (
-    <main className="flex min-h-screen  items-start justify-center" style={{ backgroundImage: "url('/christmas.avif')" }}>
+    <main className="flex min-h-screen  items-start justify-center" style={{ backgroundImage: "url('/christmas.jpg')" }}>
       <div className="max-w-5xl w-full items-center justify-start font-mono text-sm lg:flex flex-col my-6">
         <h1 className='mb-10 text-2xl'>Lista de regalos:</h1>
         <div className='mb-6 flex flex-row '>
-          <input
-            type="text"
-            placeholder="Regalo"
-            value={newGift}
-            onChange={(e) => setNewGift(e.target.value)}
-            className='text-black px-1 mx-1 w-20'
-          />
-          <input
-            type="text"
-            placeholder="URLimagen"
-            value={newImageUrl}
-            onChange={(e) => setNewImageUrl(e.target.value)}
-            className='text-black px-1 mx-1 w-20'
-          />
-          <button className='mx-1' onClick={addGift}>Agregar a la lista</button>
+          {/* Modal */}
+          {isModalOpen && (
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+              <div className="bg-white p-8 rounded-md">
+                <h2 className="text-lg mb-4">Agregar regalo</h2>
+                <input
+                  type="text"
+                  placeholder="Para quien?"
+                  value={person}
+                  onChange={(e) => setPerson(e.target.value)}
+                  className='text-black px-1 mb-2 w-full'
+                />
+                <input
+                  type="text"
+                  placeholder="Regalo"
+                  value={newGift}
+                  onChange={(e) => setNewGift(e.target.value)}
+                  className='text-black px-1 mb-2 w-full'
+                />
+                <input
+                  type="text"
+                  placeholder="URLimagen"
+                  value={newImageUrl}
+                  onChange={(e) => setNewImageUrl(e.target.value)}
+                  className='text-black px-1 mb-4 w-full'
+                />
+                <button className="bg-blue-500 text-white p-2 rounded" onClick={addGift}>
+                  Agregar
+                </button>
+                <button className="ml-4 text-gray-600" onClick={() => setIsModalOpen(false)}>
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          )}
+          <button className="m-8 p-2 text-black bg-white rounded" onClick={() => setIsModalOpen(true)}>
+            Agregar a la lista 🎁</button>
         </div>
 
         <ul className=''>
           {gifts.map((gift: string, index: number) => (
             <li className='flex flex-row items-center justify-around text-lg '
               key={index}>
-
               <div className='flex flex-row items-center justify-around bg-white w-[150px] p-2 rounded'>
+                <p className='flex w-1/2 text-semibold text-black text-sm text-start mx-2'>{gift.recipient}</p>
                 <Image
-                  loader={() => gift.imageUrl} // Return the image URL
+                  loader={() => gift.imageUrl}
                   src={gift.imageUrl}
                   alt={gift.name}
                   width={40}
